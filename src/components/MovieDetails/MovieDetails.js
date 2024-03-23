7c9d04d398c0a28c402100bffbcb3f379cea3d74
Adam Bedient <adambedient@gmail.com>
2024-03-23 10:06:51 -0400

    Format details

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -15,22 +15,42 @@ function MovieDetails ({ selectedMovie }) {
         average_rating
     } = selectedMovie
 
-    console.log("title", title)
+    const genreList = genres.reduce((acc, genre) => {
+        if (!acc) {
+            acc = genre;
+        } else {
+            acc += `, ${genre}`
+        }
+
+        return acc
+    }, "")
+
+    function formatDate(date) {
+        let formattedDate = ''
+        const splitDate = date.split('-')
+        console.log(splitDate)
+        formattedDate = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
+
+        return formattedDate
+    }
+
+    const formattedDate = formatDate(release_date)
+    
 
     return (
         <div className="modal">
             <div className="modal-content">
                 <img src={backdrop_path}></img>
                 <h2>{title}</h2>
-                <h3>{tagline}</h3>
+                <h3>"{tagline}"</h3>
                 <div className="movie-details">
                 <p className="overview">{overview}</p>
                     <div className="details--stats">
-                        <p>Release Date: {release_date}</p>
-                        <p>Genres: {genres}</p>
-                        <p>Budget: ${budget}</p>
-                        <p>Runtime: {runtime} mins</p>
-                        <p>Rating: {average_rating}</p>
+                        <p><span>Release Date: </span>{formattedDate}</p>
+                        <p><span>Genres: </span>{genreList}</p>
+                        <p><span>Budget: </span>${budget}</p>
+                        <p><span>Runtime: </span>{runtime} mins</p>
+                        <p><span>Rating: </span>{average_rating}</p>
                     </div>
                     
                 </div>
