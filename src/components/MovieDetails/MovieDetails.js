b5d135f25f2cec7e221af6f1ddd5bb096161e9ef
Adam Bedient <adambedient@gmail.com>
2024-03-23 09:52:52 -0400

    Add full movie details to modal

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,11 +1,39 @@
 import './MovieDetails.css'
 
-function MovieDetails ({ title, poster, backdrop }) {
+function MovieDetails ({ selectedMovie }) {
+    const {
+        title,
+        poster_path,
+        backdrop_path,
+        release_date,
+        overview,
+        genres,
+        budget,
+        revenue,
+        runtime,
+        tagline,
+        average_rating
+    } = selectedMovie
+
+    console.log("title", title)
+
     return (
         <div className="modal">
             <div className="modal-content">
+                <img src={backdrop_path}></img>
                 <h2>{title}</h2>
-                <img src={backdrop}></img>
+                <h3>{tagline}</h3>
+                <div className="movie-details">
+                <p className="overview">{overview}</p>
+                    <div className="details--stats">
+                        <p>Release Date: {release_date}</p>
+                        <p>Genres: {genres}</p>
+                        <p>Budget: ${budget}</p>
+                        <p>Runtime: {runtime} mins</p>
+                        <p>Rating: {average_rating}</p>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
