b498c59b6eccb22c1265ade97a844e299b3ca068
Adam Bedient <adambedient@gmail.com>
2024-03-25 18:12:28 -0400

    Remove some comments

--- cypress/e2e/homePage-spec.cy.js
+++ cypress/e2e/homePage-spec.cy.js
@@ -37,24 +37,3 @@ describe('home page', () => {
       .should('exist', '.image-card')
     })
 })
-
-
-// body: {
-//   'movies': [{
-//   "id": 694919,
-//   "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-//   "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-//   "title": "Money Plane",
-//   "average_rating": 6.666666666666667,
-//   "release_date": "2020-09-29"
-// }]      
-// }
-// })
-     // {
-      //   "id": 337401,
-      //   "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-      //   "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-      //   "title": "Mulan",
-      //   "average_rating": 4.909090909090909,
-      //   "release_date": "2020-09-04"
-      // }
