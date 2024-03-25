83d6faf18b99c954518ea740d3203665a19f7543
Adam Bedient <adambedient@gmail.com>
2024-03-25 17:08:39 -0400

    Implement heading test

--- cypress/e2e/homePage-spec.cy.js
+++ cypress/e2e/homePage-spec.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('home page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should display the title of the application', () => {
+    cy.contains("Rancid Tomatillos")
   })
 })
