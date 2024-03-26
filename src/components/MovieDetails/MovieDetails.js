8c6369846ad15c2eeba55b9c09aac2af11262d57
tconey23 <145934889+tconey23@users.noreply.github.com>
2024-03-26 17:22:37 -0600

    Adds refactored function for selected movie

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,98 +1,65 @@
 import './MovieDetails.css'
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
-function MovieDetails (/*{ selectedMovie, hideDetails }*/) {
-    
-    const id = useParams().id
-    const [selectedMovie, setSelectedMovie] = useState()
-    let fin
-    // console.log(useParams(), id)
+function MovieDetails () {
 
-    // showDetails(id)
+    const { id } = useParams()
+    const [movie, setMovie] = useState(null)
 
-    function showDetails (id) {
-        fetch (`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-        .then(resp => resp.json())
-        .then((data) => {
-          setSelectedMovie(data.movie)
-          console.log(data)
-       })
-       .then(fin => fin = setDetails())
-       .catch(err => {})
-      }
+    // const selectMovie = async (movId) => {
+    //     const movie = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movId}`)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data.movie);
+    //             setMovie(data.movie)
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching movie:', error);
+    //         });
+    // }
 
-      console.log(fin)
+    function selectMovie(movId) {
+        const movie = fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movId}`)
+            .then(response => response.json())
+            .then(data => {
+                setMovie(data.movie)
+            })
+            .catch(error => {
+                console.error('Error fetching movie:', error);
+            });
+    }
 
     useEffect(() => { 
-        console.log('made it')
-        showDetails(id)
+        selectMovie(id)
+      }, [id]) 
 
-      }, []) 
-
-
-
-    // const {
-    //     title,
-    //     poster_path,
-    //     backdrop_path,
-    //     release_date,
-    //     overview,
-    //     genres,
-    //     budget,
-    //     revenue,
-    //     runtime,
-    //     tagline,
-    //     average_rating
-    // } = selectedMovie
-
-
-const setDetails = () => {
-    const genreList = selectedMovie.genres.reduce((acc, genre) => {
-        if (!acc) {
-            acc = genre;
-        } else {
-            acc += `, ${genre}`
-        }
-
-        return acc
-    }, "")
-
-    function formatDate(date) {
-        // let formattedDate = ''
-        const splitDate = date.split('-')
-        console.log(splitDate)
-        // formattedDate = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}` 
-
-        // return formattedDate
-    
-
-
-    const formattedDate = formatDate(selectedMovie.release_date)
-
- }
     return (
             <div className="modal">
+                {movie && (    
                 <div className="modal-content">
-                    <img src={selectedMovie.backdrop_path} alt="Backdrop"></img>
-                    <h2>{selectedMovie.title}</h2>
-                    <h3>"{selectedMovie.tagline}"</h3>
+                    <Link to={`/`}>
+                        <div className="close-details">Close</div>
+                    </Link>
+                    <img src={movie.backdrop_path} alt="Backdrop"></img>
+                    <h2>{movie.title}</h2>
+                    <h3>"{movie.tagline}"</h3>
                     <div className="movie-details">
-                        <p className="overview">{selectedMovie.overview}</p>
+                        <p className="overview">{movie.overview}</p>
                         <div className="details--stats">
-                            <p><span>Release Date: </span>{}</p>
-                            <p><span>Genres: </span>{genreList}</p>
-                            <p><span>Budget: </span>${selectedMovie.budget}</p>
-                            <p><span>Runtime: </span>{selectedMovie.runtime} mins</p>
-                            <p><span>Rating: </span>{selectedMovie.average_rating}</p>
-                            <p><span>Revenue: </span>${selectedMovie.revenue}</p>
+                            <p><span>Release Date: </span>{movie.release_date}</p>
+                            <p><span>Genres: </span>{movie.genres.toString().replaceAll(',', ', ')}</p>
+                            <p><span>Budget: </span>${movie.budget}</p>
+                            <p><span>Runtime: </span>{movie.runtime} mins</p>
+                            <p><span>Rating: </span>{movie.average_rating}</p>
+                            <p><span>Revenue: </span>${movie.revenue}</p>
                         </div>
                     </div>
                 </div>
+                )}
             </div>
     );
-   }
 }
 
 // MovieDetails.propTypes = {
